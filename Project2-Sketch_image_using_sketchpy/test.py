0520e93ebd3b25968434bfa89158dee18b1bb148
rsivaraman18 <126255552+rsivaraman18@users.noreply.github.com>
2024-04-23 10:07:47 +0530

    Add files via upload

---
+++ Project2-Sketch_image_using_sketchpy/test.py
@@ -0,0 +1,11 @@
+from sketchpy import library as lib
+import random
+
+# sketchpy Library as different inbuilt class name ,those name are stored as list as shown below.
+# Now you can sketch any images from the list 
+
+Class_name = ["apj","bts","date","flag","gojo","ironman_ascii","rdj","tom_holland","vijay"]
+
+obj=lib.bts()
+obj.draw()
+
